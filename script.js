863e22a6b3e61a0420e7fa2b0b390b71b61f0271
AJ Yaseen <46060468+AJ72311@users.noreply.github.com>
2025-07-10 23:52:11 -0500

    Add scroll and load animations

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function scrollView() {
 }
 
 document.getElementById("scrollArr").addEventListener("click", scrollView);
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {            
+            const imgChildren = entry.target.querySelectorAll('img');
+            imgChildren.forEach(child => {
+                child.classList.add('visible');
+            });
+
+            const divChildren = entry.target.querySelectorAll('div');
+            divChildren.forEach(child => {
+                child.classList.add('visible');
+            });
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+document.querySelectorAll(".scroll-animate").forEach(el => {
+    observer.observe(el);
+});
